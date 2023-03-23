f2a72ce3607331a48f806d56e99da83d9c2ef8c4
Mohammed Naser <102864258+MohammedNaser28@users.noreply.github.com>
2023-03-24 00:05:23 +0200

    Remove touch based

--- Js/script.js
+++ Js/script.js
@@ -8,8 +8,6 @@ let stateCopy;
 let drag;
 let dragDateId;
 let oldPos;
-let scrollDelay = 0;
-let scrollDirection = 1;
 
 let dataToLocalStorage = {
     not_started: [],
@@ -20,12 +18,6 @@ document.addEventListener("click", () => {
     MyMenu.style.display = "none";
     stateCopy = null;
 });
-
-// document.addEventListener("keyup",(event)=>{
-//     if(event.ctrlKey && event.key === "Z"){
-
-//     }
-// })
 
 copy.addEventListener("click", () => {
     copyToCliboard(stateCopy);
@@ -70,7 +62,6 @@ function createELementsAndAppend(Ul, taskObj, contentEdit = false) {
     });
 
     p.addEventListener("keydown", (event) => {
-        // event.preventDefault();
         if (event.key === "Enter") {
             event.target.contentEditable = false;
 
@@ -123,30 +114,12 @@ function createELementsAndAppend(Ul, taskObj, contentEdit = false) {
     );
 
     li.addEventListener("dragend", (event) => {
-        // const divId = event.target.parentElement.parentElement.id;
-        // const ul = childDiv.querySelector("ul");
-        // UpdateDrop(dragDateId, divId);
         drag = null;
         dragDateId = null;
         oldPos = null;
-        // ul.append(drag);
         li.style.opacity = "1";
     });
-    li.addEventListener(
-        "touchstart",
-        (event) => {
-            event.preventDefault();
-            // drag = event.target;
-            drag = event.target;
-            dragDateId = event.target.getAttribute("data-date");
-            oldPos = event.target.parentElement.parentElement.id;
-            li.style.opacity = "0.5";
-            li.classList.add("drag-item--touchmove");
 
-            console.log("drag start");
-        },
-        false
-    );
     li.addEventListener("contextmenu", (event) => {
         event.preventDefault();
         MyMenu.style.display = "block";
@@ -155,32 +128,12 @@ function createELementsAndAppend(Ul, taskObj, contentEdit = false) {
         MyMenu.style.left = event.pageX + "px";
         stateCopy = event.target;
     });
-
-    li.addEventListener("touchmove", touchMove, false);
-    li.addEventListener("touchend", touchEnd, false);
     p.textContent = taskObj.task;
     li.setAttribute("data-date", taskObj.id);
-    console.log(li);
-    console.log(taskObj.id);
+
     if (contentEdit) {
         p.contentEditable = true;
         p.focus();
-    }
-}
-
-// function DragEnd(dateID,cul){
-
-// }
-
-function pageScroll(a, b) {
-    window.scrollBy(0, scrollDirection); // horizontal and vertical scroll increments
-    scrollDelay = setTimeout(pageScroll, 5); // scrolls every 100 milliseconds
-
-    if (a > window.innerHeight - b) {
-        scrollDirection = 1;
-    }
-    if (a < 0 + b) {
-        scrollDirection = -1 * scrollDirection;
     }
 }
 
@@ -214,80 +167,9 @@ parenDivs.forEach((childDiv) => {
         }
         const ul = childDiv.querySelector("ul");
         ul.append(drag);
-        console.log("divId", divId);
         UpdateDrop(dragDateId, divId);
     });
 });
-
-let x = 1;
-function touchMove(e) {
-    let touchLocation = e.targetTouches[0],
-        w = this.offsetWidth,
-        h = this.offsetHeight;
-    console.log(e.targetTouches[0] + "\nMEH");
-    lastMove = e;
-    touchEl = this;
-    this.style.width = w + "px";
-    this.style.height = h + "px";
-    this.style.position = "fixed";
-    this.style.left = touchLocation.clientX - w / 2 + "px";
-    this.style.top = touchLocation.clientY - h / 2 + "px";
-
-    if (
-        touchLocation.clientY > window.innerHeight - h ||
-        touchLocation.clientY < 0 + h
-    ) {
-        if (x === 1) {
-            x = 0;
-            pageScroll(touchLocation.clientY, h);
-        }
-    } else {
-        clearTimeout(scrollDelay);
-        x = 1;
-    }
-}
-
-function touchEnd(e) {
-    let box1 = this.getBoundingClientRect(),
-        x1 = box1.left,
-        y1 = box1.top,
-        h1 = this.offsetHeight,
-        w1 = this.offsetWidth,
-        b1 = y1 + h1,
-        r1 = x1 + w1;
-
-    let targets = document.querySelectorAll(".container-boxez div");
-    targets.forEach(function (target) {
-        var box2 = target.getBoundingClientRect(),
-            x2 = box2.left,
-            y2 = box2.top,
-            h2 = target.offsetHeight,
-            w2 = target.offsetWidth,
-            b2 = y2 + h2,
-            r2 = x2 + w2;
-
-        if (b1 < y2 || y1 > b2 || r1 < x2 || x1 > r2) {
-            return false;
-        } else {
-            // console.log("else1");
-            if (touchEl.classList.contains("drag-item--prepend")) {
-                target.prepend(touchEl);
-            } else {
-                const divId = e.target.parentElement.parentElement.id;
-                UpdateDrop(dragDateId, divId);
-
-                target.querySelector("ul").appendChild(touchEl);
-            }
-        }
-    });
-    drag = null;
-    dragDateId = null;
-    oldPos = null;
-    this.removeAttribute("style");
-    this.classList.remove("drag-item--touchmove");
-    clearTimeout(scrollDelay);
-    x = 1;
-}
 
 AddButtons.forEach((AddButton) => {
     AddButton.addEventListener("click", (event) => {
@@ -323,10 +205,6 @@ function read() {
         };
     return JSON.parse(json);
 }
-
-// function CheckEmpty(d){
-//     d.forEach
-// }
 
 function save(data) {
     dataToLocalStorage = data;
@@ -386,7 +264,6 @@ function UpdateItem(datadate, newUpdate) {
 function RenderFromLocalStorage(data) {
     for (const stateClass in data) {
         const cul = document.getElementById(stateClass);
-        console.log(stateClass);
         const StateUl = cul.querySelector("ul");
         data[stateClass].forEach((taskObj) => {
             createELementsAndAppend(StateUl, taskObj);
